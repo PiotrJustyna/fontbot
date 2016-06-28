42771104f9a28c2a682128a260675a545432132e
Piotr Justyna <piotr.justyna@gmail.com>
2016-06-28 20:51:10 +0100

    Tweeting with hand-made OAuth signature.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( greet
     ) where
 
+import qualified Data.ByteString.Char8 as S8
+import qualified Network.HTTP.Simple as Conduit
+import qualified Data.Yaml as Yaml
+import Data.Aeson (Value)
+import Data.ByteString.Char8 (pack)
+import System.IO
+
 greet :: IO ()
-greet = putStrLn "Hello World!"
+greet = do
+  handle <- openFile "c:\\temp\\fontbotauth.txt" ReadMode
+  contents <- hGetContents handle
+  let request = Conduit.setRequestHeader "Authorization" [pack contents] "POST https://api.twitter.com/1.1/statuses/update.json?status=Hello%20World%20From%20Haskell%20with%20OAuth%20header%20value%20taken%20from%20external%20file%21"
+  response <- Conduit.httpJSON request
+  S8.putStrLn $ Yaml.encode (Conduit.getResponseBody response :: Value)
+  hClose handle
