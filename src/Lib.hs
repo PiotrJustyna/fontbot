8bf740e68d9c4ac82173ff47a006d2ba193bcef6
Piotr Justyna <piotr.justyna@gmail.com>
2016-07-09 16:03:28 +0100

    Chained the tweet-generating functions.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( tweet
-    , tweetWithMedia
+    ( tweetWithMedia
     , printLatestHaskellTweets
     , createFontPreview
     ) where
@@ -14,20 +13,13 @@ import qualified Data.Text as Text
 import qualified Data.Yaml as Yaml
 import Codec.Picture (PixelRGBA8(..), writePng)
 import Control.Lens
+import Data.String.Conv
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
 import Graphics.Text.TrueType (loadFontFile)
 import Text.Regex.Posix
 import Web.Twitter.Conduit
 import Web.Twitter.Types
-
-tweet :: IO ()
-tweet = do
-  let status = Text.pack "Hello World from Haskell using twitter-conduit!\nhttps://github.com/himura/twitter-conduit"
-  twitterInfo <- getTwitterInfoFromEnvironment
-  manager <- newManager tlsManagerSettings
-  result <- call twitterInfo manager $ update status
-  S8.putStrLn $ Yaml.encode result
 
 tweetWithMedia :: IO ()
 tweetWithMedia = do
@@ -42,38 +34,25 @@ printLatestHaskellTweets = do
   let phrase = Text.pack "Haskell"
   twitterInfo <- getTwitterInfoFromEnvironment
   manager <- newManager tlsManagerSettings
-  result <- call twitterInfo manager $ searchTweets "Haskell" & lang ?~ "en" & count ?~ 10
+  result <- call twitterInfo manager $ searchTweets "a" & lang ?~ "en" & count ?~ 10
   parseResult result
 
 parseResult :: SearchResult [SearchStatus] -> IO ()
 parseResult (SearchResult statuses metadata) = do
-  parseMetadata metadata
   parseStatuses statuses
-
-parseMetadata :: SearchMetadata -> IO ()
-parseMetadata
-  (SearchMetadata
-    searchMetadataMaxId
-    searchMetadataSinceId
-    searchMetadataRefreshURL
-    searchMetadataNextResults
-    searchMetadataCount
-    searchMetadataCompletedIn
-    searchMetadataSinceIdStr
-    searchMetadataQuery
-    searchMetadataMaxIdStr) = do
-  putStrLn "search metadata:"
-  putStrLn $ "- query: " ++ (show searchMetadataQuery)
 
 parseStatuses :: [SearchStatus] -> IO ()
 parseStatuses [] = do return ()
 parseStatuses (x:xs) = do
+  let expression = "[ \t\r\n\v\f]+[a-zA-Z]{7,10}[ \t\r\n\v\f]+" :: String
   let extractedStatus = extractStatus x
-  let firstInterestingWord = extractedStatus =~ ("[ \t\r\n\v\f]+[a-zA-Z]{7,10}[ \t\r\n\v\f!?.,-]+" :: String)
-  putStrLn "status:"
-  putStrLn extractedStatus
-  putStrLn "first interesting word:"
-  if firstInterestingWord == "" then (parseStatuses xs) else (putStrLn firstInterestingWord)
+  let firstInterestingWord = extractedStatus =~ expression
+  let strippedFirstInterestingWord = toS . Text.strip $ Text.pack firstInterestingWord
+  putStrLn $ "status:\n\n" ++ extractedStatus ++ "\n\nfirst interesting word:"
+  if firstInterestingWord == "" then (parseStatuses xs) else do
+    putStrLn strippedFirstInterestingWord
+    createFontPreview strippedFirstInterestingWord
+    tweetWithMedia
 
 extractStatus :: SearchStatus -> String
 extractStatus
@@ -85,9 +64,9 @@ extractStatus
     searchStatusUser
     searchStatusCoordinates) = show searchStatusText
 
-createFontPreview :: IO ()
-createFontPreview = do
-  fontErr <- loadFontFile ".\\fonts\\Exo-Thin.ttf"
+createFontPreview :: String -> IO ()
+createFontPreview textToRender = do
+  fontErr <- loadFontFile ".\\fonts\\Chunkfive.ttf"
   case fontErr of
     Left err -> putStrLn err
     Right font ->
@@ -95,4 +74,4 @@ createFontPreview = do
           renderDrawing 1024 512 (PixelRGBA8 255 255 255 255)
               . withTexture (uniformTexture $ PixelRGBA8 0 0 0 255) $
                       printTextAt font (PointSize 100) (V2 150 300)
-                           "resonates"
+                           textToRender
