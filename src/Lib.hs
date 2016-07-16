f167548e72b73e6b299f590f235a57a8cd4e4c33
Piotr Justyna <piotr.justyna@gmail.com>
2016-07-16 10:51:09 +0100

    Refactored the code, fontbot no longer tweets if no interesting words found.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( tweetWithMedia
-    , printLatestHaskellTweets
-    , createFontPreview
-    , chooseRandomFont
+    ( workYourMagic
     ) where
 
 import TwitterUtilities
@@ -27,6 +24,41 @@ import Web.Twitter.Types
 
 fontPreviewPath = "font_preview.png"
 
+workYourMagic :: IO ()
+workYourMagic = do
+  let phrase = Text.pack "a"
+  twitterInfo <- getTwitterInfoFromEnvironment
+  manager <- newManager tlsManagerSettings
+  result <- call twitterInfo manager $ searchTweets phrase & lang ?~ "en" & count ?~ 10
+  let interestingWord = chooseFirstInterestingWordFromSearchResult result
+  if interestingWord == ""
+    then putStrLn "Cannot find any interesting words."
+    else do
+      fontName <- chooseRandomFont
+      createFontPreview interestingWord fontName
+      fontPreviewResult <- doesFileExist fontPreviewPath
+      if fontPreviewResult
+        then tweetWithMedia
+        else putStrLn "Cannot tweet - did not find font preview file."
+
+chooseFirstInterestingWordFromSearchResult :: SearchResult [SearchStatus] -> String
+chooseFirstInterestingWordFromSearchResult (SearchResult statuses metadata) = chooseFirstInterestingWordFromSearchStatuses statuses
+
+chooseFirstInterestingWordFromSearchStatuses :: [SearchStatus] -> String
+chooseFirstInterestingWordFromSearchStatuses [] = ""
+chooseFirstInterestingWordFromSearchStatuses (x:xs) =
+  if strippedFirstInterestingWord == ""
+    then chooseFirstInterestingWordFromSearchStatuses xs
+    else strippedFirstInterestingWord
+  where
+    expression = "[ \t\r\n\v\f]+[a-zA-Z]{7,10}[ \t\r\n\v\f]+" :: String
+    extractedStatus = extractStatus x
+    firstInterestingWord = extractedStatus =~ expression
+    strippedFirstInterestingWord = toS . Text.strip $ Text.pack firstInterestingWord
+
+extractStatus :: SearchStatus -> String
+extractStatus (SearchStatus searchStatusCreatedAt searchStatusId searchStatusText searchStatusSource searchStatusUser searchStatusCoordinates) = toS searchStatusText
+
 chooseRandomFont :: IO String
 chooseRandomFont = do
   paths <- getDirectoryContents ".\\fonts\\"
@@ -34,50 +66,13 @@ chooseRandomFont = do
   randomIndex <- randomRIO (0, (length foundFonts) - 1)
   return $ foundFonts !! randomIndex
 
-tweetWithMedia :: IO ()
-tweetWithMedia = do
-  let status = Text.pack ""
-  twitterInfo <- getTwitterInfoFromEnvironment
-  manager <- newManager tlsManagerSettings
-  result <- call twitterInfo manager $ updateWithMedia status (MediaFromFile ".\\font_preview.png")
-  S8.putStrLn $ Yaml.encode result
-
-printLatestHaskellTweets :: IO ()
-printLatestHaskellTweets = do
-  let phrase = Text.pack "a"
-  twitterInfo <- getTwitterInfoFromEnvironment
-  manager <- newManager tlsManagerSettings
-  result <- call twitterInfo manager $ searchTweets phrase & lang ?~ "en" & count ?~ 10
-  parseResult result
-
-parseResult :: SearchResult [SearchStatus] -> IO ()
-parseResult (SearchResult statuses metadata) = parseStatuses statuses
-
-parseStatuses :: [SearchStatus] -> IO ()
-parseStatuses [] = do return ()
-parseStatuses (x:xs) = do
-  let expression = "[ \t\r\n\v\f]+[a-zA-Z]{7,10}[ \t\r\n\v\f]+" :: String
-  let extractedStatus = extractStatus x
-  let firstInterestingWord = extractedStatus =~ expression
-  let strippedFirstInterestingWord = toS . Text.strip $ Text.pack firstInterestingWord
-  if strippedFirstInterestingWord == "" then (parseStatuses xs) else do
-    fontName <- chooseRandomFont
-    createFontPreview strippedFirstInterestingWord fontName
-    fontPreviewResult <- doesFileExist fontPreviewPath
-    if (fontPreviewResult)
-      then tweetWithMedia
-      else putStrLn "Cannot tweet - did not find font preview file."
-
-extractStatus :: SearchStatus -> String
-extractStatus (SearchStatus searchStatusCreatedAt searchStatusId searchStatusText searchStatusSource searchStatusUser searchStatusCoordinates) = toS searchStatusText
-
 createFontPreview :: String -> String -> IO ()
 createFontPreview textToRender fontName = do
   fontLoadResult <- loadFontFile $ ".\\fonts\\" ++ fontName
   case fontLoadResult of
     Left errorDescription -> do
       isFileFound <- doesFileExist fontPreviewPath
-      if (isFileFound)
+      if isFileFound
         then do
           removeFile fontPreviewPath
           putStrLn ("Cannot load font \"" ++ fontName ++ "\". Reason: " ++ errorDescription)
@@ -87,3 +82,11 @@ createFontPreview textToRender fontName = do
       renderDrawing 1024 512 (PixelRGBA8 255 255 255 255) .
       withTexture (uniformTexture $ PixelRGBA8 0 0 0 255) $
       printTextAt font (PointSize 100) (V2 150 300) textToRender
+
+tweetWithMedia :: IO ()
+tweetWithMedia = do
+  let status = Text.pack ""
+  twitterInfo <- getTwitterInfoFromEnvironment
+  manager <- newManager tlsManagerSettings
+  result <- call twitterInfo manager $ updateWithMedia status (MediaFromFile fontPreviewPath)
+  S8.putStrLn $ Yaml.encode result
